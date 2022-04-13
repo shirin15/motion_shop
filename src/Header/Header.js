8277bc5b41f07baaf1ccbd5d16f501ec915d05c1
shirin15 <imengazyevashika@gmail.com>
2022-04-13 15:36:09 +0300

    fbdbd

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div className="container">
             <h1>header</h1>
             <h1>header</h1>
             <h1>header</h1>
