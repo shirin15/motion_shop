23a834fdc902e910e3d31b117a7b29a8a09390bd
shirin15 <imengazyevashika@gmail.com>
2022-04-13 15:30:47 +0300

    fdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
-      shirin | \| rolan
+      <Header/>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>header</h1>
+            <h1>header</h1>
+            <h1>header</h1>
+            <h1>header</h1>
+        </div>
+    );
+};
+
+export default Header;
